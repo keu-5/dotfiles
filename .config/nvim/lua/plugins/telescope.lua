2407ddeff2a5707e979fd247212b886ca596867e
keu-5 <keu02.78@gmail.com>
2025-07-27 18:03:59 +0900

    feat: add telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,143 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            cond = function()
+                return vim.fn.executable("make") == 1
+            end,
+        },
+        "nvim-telescope/telescope-ui-select.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    cmd = "Telescope",
+    keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "ファイルを検索" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "文字列を検索" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "バッファを検索" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "ヘルプを検索" },
+        { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "最近のファイル" },
+        { "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "カラースキーム" },
+        { "<leader>fm", "<cmd>Telescope marks<cr>", desc = "マークを検索" },
+        { "<leader>fj", "<cmd>Telescope jumplist<cr>", desc = "ジャンプリスト" },
+        { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "カーソル下の文字列を検索" },
+        { "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "現在のバッファ内を検索" },
+    },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
+
+        telescope.setup({
+            defaults = {
+                prompt_prefix = " ",
+                selection_caret = " ",
+                path_display = { "truncate" },
+                sorting_strategy = "ascending",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        preview_width = 0.55,
+                    },
+                    vertical = {
+                        mirror = false,
+                    },
+                    width = 0.87,
+                    height = 0.80,
+                    preview_cutoff = 120,
+                },
+                mappings = {
+                    i = {
+                        ["<C-n>"] = actions.cycle_history_next,
+                        ["<C-p>"] = actions.cycle_history_prev,
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-c>"] = actions.close,
+                        ["<Down>"] = actions.move_selection_next,
+                        ["<Up>"] = actions.move_selection_previous,
+                        ["<CR>"] = actions.select_default,
+                        ["<C-x>"] = actions.select_horizontal,
+                        ["<C-v>"] = actions.select_vertical,
+                        ["<C-t>"] = actions.select_tab,
+                        ["<C-u>"] = actions.preview_scrolling_up,
+                        ["<C-d>"] = actions.preview_scrolling_down,
+                        ["<PageUp>"] = actions.results_scrolling_up,
+                        ["<PageDown>"] = actions.results_scrolling_down,
+                        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        ["<C-l>"] = actions.complete_tag,
+                        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+                    },
+                    n = {
+                        ["<esc>"] = actions.close,
+                        ["<CR>"] = actions.select_default,
+                        ["<C-x>"] = actions.select_horizontal,
+                        ["<C-v>"] = actions.select_vertical,
+                        ["<C-t>"] = actions.select_tab,
+                        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        ["j"] = actions.move_selection_next,
+                        ["k"] = actions.move_selection_previous,
+                        ["H"] = actions.move_to_top,
+                        ["M"] = actions.move_to_middle,
+                        ["L"] = actions.move_to_bottom,
+                        ["<Down>"] = actions.move_selection_next,
+                        ["<Up>"] = actions.move_selection_previous,
+                        ["gg"] = actions.move_to_top,
+                        ["G"] = actions.move_to_bottom,
+                        ["<C-u>"] = actions.preview_scrolling_up,
+                        ["<C-d>"] = actions.preview_scrolling_down,
+                        ["<PageUp>"] = actions.results_scrolling_up,
+                        ["<PageDown>"] = actions.results_scrolling_down,
+                        ["?"] = actions.which_key,
+                    },
+                },
+            },
+            pickers = {
+                find_files = {
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                },
+                live_grep = {
+                    additional_args = function(opts)
+                        return { "--hidden" }
+                    end
+                },
+                buffers = {
+                    previewer = false,
+                    layout_config = {
+                        width = 80,
+                    },
+                },
+                oldfiles = {
+                    prompt_title = "Recent Files",
+                },
+                colorscheme = {
+                    enable_preview = true,
+                },
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                },
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown({
+                        winblend = 10,
+                    }),
+                },
+            },
+        })
+
+        -- 拡張を読み込み
+        pcall(telescope.load_extension, "fzf")
+        pcall(telescope.load_extension, "ui-select")
+    end,
+}
